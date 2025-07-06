797d0d202437eca8de769ce461cb5e1a82c8c9e7
Paloma <paloma.sq@hotmail.com>
2025-07-06 10:57:46 -0300

    refactor: update project name and metadata to "Novak & Gouveia" in package files and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,21 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Novak & Gouveia",
+  description:
+    "Gerenciador da consutoria, visualise o seu processo com o nosso dashboard",
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
